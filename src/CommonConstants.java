7ec619b6531805e6341e82d3af6521caa2d6215a
anamika <kavita.nbc.9876@gmail.com>
2025-04-04 22:51:05 +0530

    added button and scrollbar

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -5,6 +5,12 @@ public class CommonConstants {
     public static final Dimension GUI_SIZE = new Dimension(540,760);
 
     //banner config
-    public static final Dimension Banner_Size = new Dimension(GUI_SIZE.width, 50);
+    public static final Dimension BANNER_SIZE = new Dimension(GUI_SIZE.width, 50);
+
+    //task panel config
+    public static final Dimension TASKPANEL_SIZE = new Dimension(GUI_SIZE.width-30, GUI_SIZE.height-175);
+
+    //add task button config
+    public static final Dimension ADDTASK_BUTTON_SIZE= new Dimension(GUI_SIZE.width,50);
 
 }
--- src/ToDoListGui.java
+++ src/ToDoListGui.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 
 public class ToDoListGui extends JFrame {
+    //taskPanel will act as a container for the task component
+    //taskComponentPanel will store all the TaskComponent
+    private JPanel taskPanel , taskComponentPanel;
     public ToDoListGui(){
         super("To Do List Application");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -18,12 +21,33 @@ public class ToDoListGui extends JFrame {
         bannerLabel.setBounds(
                 (CommonConstants.GUI_SIZE.width - bannerLabel.getPreferredSize().width)/2,
                 15,
-                CommonConstants.Banner_Size.width,
-                CommonConstants.Banner_Size.height
+                CommonConstants.BANNER_SIZE.width,
+                CommonConstants.BANNER_SIZE.height
         );
 
+        //taskPanel
+        taskPanel = new JPanel();
+
+        //taskComponentPanel
+        taskComponentPanel = new JPanel();
+        taskComponentPanel.setLayout(new BoxLayout(taskComponentPanel,BoxLayout.Y_AXIS));
+        taskPanel.add(taskComponentPanel);
+
+        //add scrolling to the taskPanel
+
+        JScrollPane scrollPane = new JScrollPane(taskPanel);
+        scrollPane.setBounds(8, 70 , CommonConstants.TASKPANEL_SIZE.width, CommonConstants.TASKPANEL_SIZE.height);
+        scrollPane.setMaximumSize(CommonConstants.TASKPANEL_SIZE);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        //add task button
+        JButton addTaskButton = new JButton("Add Task");
+        addTaskButton.setBounds(-5, CommonConstants.GUI_SIZE.height-88,
+                CommonConstants.ADDTASK_BUTTON_SIZE.width,CommonConstants.ADDTASK_BUTTON_SIZE.height);
         //add to frame
         this.getContentPane().add(bannerLabel);
-
+        this.getContentPane().add(scrollPane);
+        this.getContentPane().add(addTaskButton);
     }
 }
